d19f7046428d85a0788ceb120351470f43058213
Stefan <stefan.dokic@saboit.de>
2022-11-02 20:45:51 +0100

    Test Meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,33 @@
 import '../styles/index.scss'
+import Script from 'next/script'
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
-    <html>
-      <head></head>
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Blue Haven</title>
+        <meta
+          name="description"
+          content="Astrology Purpose Overview and Breakthrough Coaching. Change your life and find your purpose!"
+        />
+        <meta name="robots" content="index, follow" />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LRM2WR3R6E"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LRM2WR3R6E');
+        `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
